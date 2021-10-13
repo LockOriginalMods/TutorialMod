dc67a3d9b2d3d739e3742d54328842a132ce3d80
LockOriginalMods <81614998+LockOriginalMods@users.noreply.github.com>
2021-10-13 21:25:25 +0300

    Delete src/main directory

--- src/main/java/com/LockOriginalMods/tutorialmod/list/FoodList.java
+++
@@ -1,9 +0,0 @@
-package com.LockOriginalMods.tutorialmod.list;
-
-import net.minecraft.world.food.FoodProperties;
-
-public class FoodList {
-
-    public static final FoodProperties PEPPER = (new FoodProperties.Builder()).nutrition(6).saturationMod(0.6F).meat().build();
-
-}
